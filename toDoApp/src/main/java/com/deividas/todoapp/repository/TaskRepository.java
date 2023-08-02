bc9f55db60d28e6ad84403d9a55c45c91623da5c
Deividas Kvietkauskas <65194251+DvDs20@users.noreply.github.com>
2023-08-02 22:15:27 +0300

    Delete toDoApp directory

--- toDoApp/src/main/java/com/deividas/todoapp/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.deividas.todoapp.repository;
-
-import com.deividas.todoapp.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-
-}
--- toDoApp/src/main/resources/db.changelog/scripts/v001-Create-to_do-schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE SCHEMA if NOT EXISTS to_do
-    AUTHORIZATION admin;
-
-COMMENT ON SCHEMA to_do
-    IS 'To-Do app schema';
